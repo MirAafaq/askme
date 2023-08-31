c90ea202df991509c76d20a6fbf7967db1c4c73b
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 19:51:45 +0530

    Update AskCss.php

--- src/AskCss.php
+++ src/AskCss.php
@@ -1,64 +1,73 @@
-<?php 
+<?php
+
 namespace AskMe;
 
-class CoreCss{
- public function applyCss(){
-
-
-  $css_code = "<style>
- form {
-    width: fit-content;
-    text-align: start;
-    margin: 20px;
-    font-family: calibri;
-    box-shadow: 1px 10px 40px lightcoral;
-    background: aliceblue;
-    border-radius: 8px;
-    padding: 10px;
+class CoreCss
+{
+  public function applyCss()
+  {
+    $css_code = <<<EOF
+/* The .mdbn class is used to make text monospaced */
+.mdbn {
+  display: contents;
+  white-space: pre-line;
 }
+
+form {
+  width: fit-content;
+  text-align: start;
+  margin: 20px;
+  font-family: calibri;
+  box-shadow: 1px 10px 40px lightcoral;
+  background: aliceblue;
+  border-radius: 8px;
+  padding: 10px;
+}
+
 input {
-    margin: 8px;
-    box-shadow: 1px 10px 40px lightcoral;
-    border-radius: 8px;
-   
-    width: -webkit-fill-available;
-    transition: border-color 0.3s; 
-    border:none;
-    padding: 10px;
-  
+  margin: 8px;
+  box-shadow: 1px 10px 40px lightcoral;
+  border-radius: 8px;
+  width: -webkit-fill-available;
+  transition: border-color 0.3s;
+  border: none;
+  padding: 10px;
 }
-
 
 textarea {
-    border: none;
-    border-radius: 8px;
-    width: -webkit-fill-available;
-    margin: 4px;
-    padding:4px;
-     box-shadow: 1px 10px 40px lightcoral;
+  border: none;
+  border-radius: 8px;
+  width: -webkit-fill-available;
+  margin: 4px;
+  padding: 4px;
+  box-shadow: 1px 10px 40px lightcoral;
 }
-textarea:focus{
-     outline: none;
+
+textarea:focus {
+  outline: none;
 }
+
 /* Style for the input when focused */
 input:focus {
-  border-color: green; /* Change border color when focused */
-  outline: none; /* Remove the default focus outline */
- 
+  border-color: green;
+  outline: none;
 }
+
 button {
-    padding: 10px;
-    margin: 4px;
-    font-weight: 800;
-     box-shadow: 1px 10px 40px lightcoral;
-    border: none;
-    border-radius: 6px;
+  padding: 10px;
+  margin: 4px;
+  font-weight: 800;
+  box-shadow: 1px 10px 40px lightcoral;
+  border: none;
+  border-radius: 6px;
 }
-button:hover{
-    background:#000;
-      box-shadow: 1px 10px 40px #fff;
-    color:#fff;
+
+button:hover {
+  background: #000;
+  box-shadow: 1px 10px 40px #fff;
+  color: #fff;
 }
+
 /* Styling for select field */
 select {
   appearance: none;
@@ -71,6 +80,7 @@ select {
   max-width: 300px;
   background-color: white;
 }
+
 /* Styling for radio button container */
 .radio-container {
   display: flex;
@@ -81,58 +91,53 @@ select {
 /* Styling for radio button */
 input[type='radio'] {
   appearance: none;
-  width:20px;
-  padding:8px;
-  border: 2px solid #007bff; /* Change to your desired color */
- 
+  width: 20px;
+  padding: 8px;
+  border: 2px solid #007bff;
   margin: 0;
   cursor: pointer;
   outline: none;
 }
 
 /* Styling for selected radio button */
- input[type='radio']:checked {
-  background-color: #e97688; /* Change to your desired color */
+input[type='radio']:checked {
+  background-color: #e97688;
 }
 
 /* Hide the default radio button */
- input[type='radio']::before {
+input[type='radio']::before {
   content: '';
   display: inline-block;
- 
- 
 }
 
 /* Optional: Adding a label for radio button */
 .radio-label {
   font-size: 14px;
 }
+
 /* Styling for checkbox container */
 
 /* Styling for checkbox input */
- input[type='checkbox'] {
+input[type='checkbox'] {
   appearance: none;
-  width:20px;
-  padding:8px;
-
+  width: 20px;
+  padding: 8px;
   border: none; /* Change to your desired color */
   border-radius: 4px;
-
   margin: 4px;
   cursor: pointer;
   outline: none;
 }
 
 /* Styling for selected checkbox */
- input[type='checkbox']:checked {
- background:#459; /* Change to your desired color */
+input[type='checkbox']:checked {
+  background: #459; /* Change to your desired color */
 }
 
 /* Hide the default checkbox */
 input[type='checkbox']::before {
   content: '';
   display: inline-block;
-
   border-radius: 4px;
 }
 
@@ -140,15 +145,9 @@ input[type='checkbox']::before {
 .checkbox-label {
   font-size: 14px;
 }
-
-.mdbn {
-    display: contents;
-    white-space: pre-line;
+EOF;
+    return $css_code;
+  }
 }
-</style>";
- return $css_code;
 
- }
-
-}
 ?>
