0ce567e4d709cbf18a5e9005d5fef581e15873d1
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 15:29:02 +0530

    Create SelectField.php

---
+++ src/Field/SelectField.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AskMe\Field;
+
+class SelectField extends AbstractField
+{
+    private $options;
+
+    public function __construct($name, $options)
+    {
+        parent::__construct($name);
+        $this->options = $options;
+    }
+
+    public function render()
+    {
+        $html = '<label for="' . $this->name . '">' . ucfirst($this->name) . ':</label>
+                <select name="' . $this->name . '">';
+
+        foreach ($this->options as $value => $label) {
+            $html .= '<option value="' . $value . '">' . $label . '</option>';
+        }
+
+        $html .= '</select><br>';
+
+        return $html;
+    }
+}
