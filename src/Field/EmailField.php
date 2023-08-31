3ffeb5a4d26cf56b126a28aad9b5a2b8f0f7eef8
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 18:59:44 +0530

    Update EmailField.php

--- src/Field/EmailField.php
+++ src/Field/EmailField.php
@@ -7,7 +7,7 @@ class EmailField extends AbstractField
     public function render()
     {
         $html = '<label for="' . $this->name . '" class="block font-medium mb-1">' . ucfirst($this->name) . ':</label>';
-        $html .= '<input type="email" name="' . $this->name . '" class="w-full p-2 border rounded focus:outline-none focus:border-blue-500"><br>';
+        $html .= '<input type="email" name="' . $this->name . '" placeholder="' . $this->name . '"><br>';
 
         return $html;
     }
