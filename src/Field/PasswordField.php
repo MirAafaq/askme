5bbc796ea3c0d714a728a0399007ed09dc395479
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 15:31:06 +0530

    Create PasswordField.php

---
+++ src/Field/PasswordField.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AskMe\Field;
+
+class PasswordField extends AbstractField
+{
+    public function render()
+    {
+        return '<label for="' . $this->name . '">' . ucfirst($this->name) . ':</label>
+                <input type="password" name="' . $this->name . '"><br>';
+    }
+}
