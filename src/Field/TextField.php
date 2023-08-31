38efb77747d0cf3e28ade2e02f342f292aad3762
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 15:30:13 +0530

    Create TextField.php

---
+++ src/Field/TextField.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AskMe\Field;
+
+class TextField extends AbstractField
+{
+    public function render()
+    {
+        return '<label for="' . $this->name . '">' . ucfirst($this->name) . ':</label>
+                <input type="text" name="' . $this->name . '"><br>';
+    }
+}
