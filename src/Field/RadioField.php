c3dcb335d38a1287014c9cf338d16a32a6cefa56
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 15:28:20 +0530

    Create RadioField.php

---
+++ src/Field/RadioField.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AskMe\Field;
+
+class RadioField extends AbstractField
+{
+    private $options;
+
+    public function __construct($name, $options)
+    {
+        parent::__construct($name);
+        $this->options = $options;
+    }
+
+    public function render()
+    {
+        $html = '<div><label>' . ucfirst($this->name) . ':</label><br>';
+
+        foreach ($this->options as $value => $label) {
+            $html .= '<label for="' . $this->name . '_' . $value . '">
+                        <input type="radio" name="' . $this->name . '" value="' . $value . '"> ' . $label . '
+                    </label><br>';
+        }
+
+        $html .= '</div>';
+
+        return $html;
+    }
+}
