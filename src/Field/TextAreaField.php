85195c5842dfce9c3dbe3e1fe85bba8d3e88a705
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2023-08-31 18:58:53 +0530

    Update TextAreaField.php

--- src/Field/TextAreaField.php
+++ src/Field/TextAreaField.php
@@ -6,6 +6,6 @@ class TextAreaField extends AbstractField
     public function render()
     {
         return '<label for="' . $this->name . '">' . ucfirst($this->name) . ':</label>
-                <textarea name="' . $this->name . '"></textarea><br>';
+                <textarea name="' . $this->name . '">Enter Your Text ....</textarea><br>';
     }
 }
