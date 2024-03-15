273f3cfa700ac5d2a42d9805cad004d3ea090559
Aafaq Ahmad  Mir <133475194+MirAafaq@users.noreply.github.com>
2024-03-16 02:27:10 +0530

    Update PasswordField.php

--- src/Field/PasswordField.php
+++ src/Field/PasswordField.php
@@ -6,7 +6,6 @@ class PasswordField extends AbstractField
 {
     public function render()
     {
-        return '<label for="' . $this->name . '">' . ucfirst($this->name) . ':</label>
-                <input type="password" name="' . $this->name . '" placeholder="' . $this->name . '"><br>';
+        return '<label for="' . $this->name . '">' . ucfirst($this->name) . ':</label><input type="password" name="' . $this->name . '" placeholder="' . $this->name . '"><br>';
     }
 }
